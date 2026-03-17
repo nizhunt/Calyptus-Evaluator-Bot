be3227d3f7d09016a08fad369612d66a72c3e05b
NISHANT SINGH <NISHANTTHESINGH@GMAIL.COM>
2026-03-17 16:21:02 +0530

    Add favicon assets and link them in the document head

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Reenie+Beanie&display=swap"
           rel="stylesheet"
